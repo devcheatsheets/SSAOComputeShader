7b6a479404ab87ce623f9e6d2a3a5d9d15d8d4ea
danshevchuk <dan@strawbfields.com>
2024-02-06 21:38:56 -0500

    initial commit

---
+++ Assets/Code/SSAOcpu.cs
@@ -0,0 +1,174 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SSAOcpu : MonoBehaviour
+{
+    public ComputeShader ssaoShader;
+    public ComputeShader blurShader;
+
+
+    [Header("SSAO Settings")]
+    [SerializeField] private float _totalStrength = 0.2f;
+    [SerializeField] private float _area = 1.0f;
+    [SerializeField] private float _falloff = 0.1f;
+    [SerializeField] private float _radius = 0.001f;
+    [SerializeField] private float _bias = 0.1f;
+    [SerializeField] private bool _debugMode = false;
+
+    [Header("Blur Settings")]
+    [SerializeField] int _blurKernelSize = 4;
+    [SerializeField] float _blurTexelSizeMultiplier = 1.0f;
+    [SerializeField] float _blurResultDivider = 50.0f;
+
+    private List<float> _samples;
+
+    private int _ssaoKernel;
+    private int _blurKernel;
+
+    private Texture2D _ssaoNoise;
+
+    private Camera _camera;
+    private RenderTexture _ssaoResultRenderTexture;
+    private RenderTexture _blurResultRenderTexture;
+
+    private void InitCamera()
+    {
+        _camera = GetComponent<Camera>();
+        _camera.depthTextureMode = _camera.depthTextureMode | DepthTextureMode.DepthNormals;
+    }
+
+    private void InitRenderTextures()
+    {
+        if (_ssaoResultRenderTexture == null)
+        {
+            _ssaoResultRenderTexture = new RenderTexture(_camera.pixelWidth, _camera.pixelHeight, 24);
+            _ssaoResultRenderTexture.enableRandomWrite = true;
+            _ssaoResultRenderTexture.Create();
+        }
+
+        if (_blurResultRenderTexture == null)
+        {
+            _blurResultRenderTexture = new RenderTexture(_camera.pixelWidth, _camera.pixelHeight, 24);
+            _blurResultRenderTexture.enableRandomWrite = true;
+            _blurResultRenderTexture.Create();
+        }
+    }
+
+    private void InitNoiseTexture()
+    {
+        _ssaoNoise = new Texture2D(4, 4);
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                Color randomColor = new Color(
+                    UnityEngine.Random.Range(0f, 1f),
+                    UnityEngine.Random.Range(0f, 1f),
+                    0f,
+                    1f
+                );
+                _ssaoNoise.SetPixel(i, j, randomColor);
+            }
+        }
+
+        _ssaoNoise.wrapMode = TextureWrapMode.Repeat;
+        _ssaoNoise.Apply(false);
+    }
+
+    private void InitSamples()
+    {
+        _samples = new List<float>();
+        for (int i = 0; i < 128; i++)
+        {
+            var x = UnityEngine.Random.Range(0f, 1f) * 2.0f - 1.0f;
+            var y = UnityEngine.Random.Range(0f, 1f) * 2.0f - 1.0f;
+            var z = UnityEngine.Random.Range(0f, 1f);
+            var v = new Vector3(x, y, z).normalized;
+            v *= UnityEngine.Random.Range(-1f, 1f);
+            float scale = (float)i / 128.0f;
+            scale = Mathf.Lerp(0.1f, 1.0f, scale * scale);
+            v *= scale;
+            _samples.Add(v.x);
+            _samples.Add(v.y);
+            _samples.Add(v.z);
+        }
+    }
+
+    private void InitKernels()
+    {
+        _ssaoKernel = ssaoShader.FindKernel("CSMain");
+        _blurKernel = blurShader.FindKernel("CSMain");
+    }
+
+    private void Awake()
+    {
+        InitCamera();
+        InitRenderTextures();
+        InitNoiseTexture();
+        InitSamples();
+        InitKernels();
+    }
+
+    private void UpdateSSAOShaderValues(RenderTexture src)
+    {
+        ssaoShader.SetTexture(_ssaoKernel, "Src", src);
+        ssaoShader.SetTexture(_ssaoKernel, "Result", _ssaoResultRenderTexture);
+
+        ssaoShader.SetFloats("samples", _samples.ToArray());
+
+        ssaoShader.SetTextureFromGlobal(_ssaoKernel, "_CameraDepthNormalsTexture", "_CameraDepthNormalsTexture");
+        ssaoShader.SetTextureFromGlobal(_ssaoKernel, "_CameraDepthTexture", "_CameraDepthTexture");
+        ssaoShader.SetTexture(_ssaoKernel, "RandomTextureSampler", _ssaoNoise);
+
+        ssaoShader.SetFloat("totalStrength", _totalStrength);
+        ssaoShader.SetFloat("area", _area);
+        ssaoShader.SetFloat("falloff", _falloff);
+        ssaoShader.SetFloat("radius", _radius * _camera.pixelWidth / 10);
+        ssaoShader.SetFloat("bias", _bias);
+
+        ssaoShader.SetFloat("width", _camera.pixelWidth);
+        ssaoShader.SetFloat("height", _camera.pixelHeight);
+
+        var matrix = Matrix4x4.Perspective(_camera.fieldOfView, _camera.aspect, _camera.nearClipPlane, _camera.farClipPlane);
+        var shaderMatrix = GL.GetGPUProjectionMatrix(matrix, false);
+        ssaoShader.SetMatrix("projection", shaderMatrix);
+        ssaoShader.SetMatrix("matInvProjection", shaderMatrix.inverse);
+        var viewMatrixInv = _camera.worldToCameraMatrix.inverse;
+        var shaderViewMatrixInv = GL.GetGPUProjectionMatrix(viewMatrixInv, false);
+        ssaoShader.SetMatrix("viewMatrixInv", shaderViewMatrixInv);
+
+        ssaoShader.SetBool("debugMode", _debugMode);
+    }
+
+    private void UpdateBlurShaderValues(RenderTexture src)
+    {
+        blurShader.SetTexture(_blurKernel, "Src", src);
+        blurShader.SetTexture(_blurKernel, "ssaoInput", _ssaoResultRenderTexture);
+        blurShader.SetTexture(_blurKernel, "Result", _blurResultRenderTexture);
+        blurShader.SetFloat("width", _camera.pixelWidth);
+        blurShader.SetFloat("height", _camera.pixelHeight);
+        blurShader.SetBool("debug", _debugMode);
+        blurShader.SetInt("blurKernelSize", _blurKernelSize);
+        blurShader.SetFloat("blurTexelSizeMultiplier", _blurTexelSizeMultiplier);
+        blurShader.SetFloat("blurResultDivider", _blurResultDivider);
+    }
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        UpdateSSAOShaderValues(src);
+        ssaoShader.Dispatch(_ssaoKernel, _ssaoResultRenderTexture.width / 8, _ssaoResultRenderTexture.height / 8, 1);
+
+        UpdateBlurShaderValues(src);
+        blurShader.Dispatch(_blurKernel, _blurResultRenderTexture.width / 8, _blurResultRenderTexture.height / 8, 1);
+
+        Graphics.Blit(_blurResultRenderTexture, dest);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _debugMode = !_debugMode;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,68 @@
+Unity version 2022.3.14f1
+
+**SSAO Compute Shader**
+
+**Unity | C# | Shaders | HLSL | Compute Shader**
+
+![ssao](Documentation/ssao-main.gif)
+
+**SSAO Compute Shader** is a great example of using a compute shader to create a Screen Space Ambient Occlusion effect in Unity. [[wiki]](https://en.wikipedia.org/wiki/Screen_space_ambient_occlusion)
+
+Compute shaders are a type of shader used in graphics programming for general-purpose computing on graphics processing units (GPUs). Unlike traditional shaders, such as vertex or fragment shaders, which are primarily used for rendering graphics, compute shaders are designed for parallel computing tasks. They allow developers to perform highly parallel computation on the GPU, making them well-suited for a variety of non-graphics tasks.
+
+You can learn more about compute shaders [here](https://docs.unity3d.com/Manual/class-ComputeShader.html).
+
+---
+
+The main logic is contained within the script called *SSAOcpu.cs* (Under Assets/Code). Here's a step-by-step explanation of how it works:
+
+**1. Initialization Phase:**
+
+* The script initializes the camera and sets it up to capture depth and normal information, which is crucial for ambient occlusion calculations.
+* It creates two RenderTextures to store the intermediate results of the SSAO and blur operations.
+* A 4x4 texture with random colors is generated to introduce randomness into the SSAO calculations.
+* A list of 128 random vectors is created and used as samples for the SSAO algorithm.
+* The script retrieves the indices of compute shader kernels for both the SSAO and blur shaders.
+
+**2. SSAO Shader Setup:**
+
+* The script defines various parameters that control the SSAO effect, such as strength, base, area, falloff, radius, bias, threshold, and a debug mode toggle.
+
+**3. UpdateSSAOShaderValues Method:**
+
+* This method is called before dispatching the SSAO compute shader.
+* It sets up textures, parameters, and data needed for the SSAO computation.
+* Matrices related to camera projection and view are set to ensure proper transformation.
+* The SSAO compute shader is dispatched to perform the actual computation.
+
+**4. Blur Shader Setup:**
+
+* The script defines parameters for the blur effect, such as kernel size, texel size multiplier, and result divider.
+
+**5. UpdateBlurShaderValues Method:**
+
+* This method is called before dispatching the blur compute shader.
+* It sets up textures, parameters, and data needed for the blur computation.
+* The blur compute shader is dispatched to apply the blur effect to the SSAO result.
+
+**6. OnRenderImage Method:**
+
+* This method is automatically called by Unity during the rendering process.
+* It calls UpdateSSAOShaderValues to set up SSAO shader parameters and dispatches the SSAO compute shader.
+* It calls UpdateBlurShaderValues to set up blur shader parameters and dispatches the blur compute shader.
+* The final blurred result is applied to the destination RenderTexture, which is the image being rendered.
+* In summary, the script initializes necessary components, sets up shaders for SSAO and blur, and then, during the rendering process, it dispatches the shaders to compute and apply ambient occlusion, resulting in a visually improved scene with realistic shadows.
+
+---
+
+The SSAOcpu component is attached to the Camera in the Main scene.
+![hierarchy](Documentation/hierarchy.png) 
+
+You can see and change the parameters in Inspector.
+![inspector](Documentation/inspector.png)
+
+**Navigation**
+Use your mouse while holding the right mouse button to rotate the camera.
+Use **WASD** to move. Use **E** to move up and **Q** to move down.
+
+Press **Space** to toggle the *debug mode* on and off.
